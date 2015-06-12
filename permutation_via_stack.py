d801bd4a6d98e90b2d85af9221d23463dc76d3ae
Peter Teoh <htmldeveloper@gmail.com>
2015-06-12 09:26:48 +0800

    new file:   permutation_via_stack.py

---
+++ permutation_via_stack.py
@@ -0,0 +1,17 @@
+
+
+def perms(word):
+    stack = list(word)
+    results = [stack.pop()]
+    while len(stack) != 0:
+        c = stack.pop()
+        new_results = []
+        for w in results:
+            for i in range(len(w)+1):
+                new_results.append(w[:i] + c + w[i:])
+        results = new_results
+    return results
+
+##word=[1,2,3,4]
+word="1234"
+print perms(word)
